1f3c9f1b62e85d3b6f5de4924ef129fc7c51b2e1
Thales <thalesquadros2@gmail.com>
2024-03-22 10:22:31 -0300

    secret number project coded in C language

---
+++ Guessing_game.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("----------------------------------- \n");
+    printf("welcome to the guessing game \n");
+    printf("----------------------------------- \n");
+
+    int secret_number = 47;
+
+    int attempts = 0;
+    int kick;
+    int att_num_defined;
+    
+    printf("Define the number of attempts \n");
+    scanf("%d", &att_num_defined);
+
+    printf("----------------------------------- \n");
+
+    while (attempts < att_num_defined) {
+        printf("Insert an attempt for secret number \n");
+        scanf("%d", &kick);
+        printf("Your attempt was %d \n", kick);
+        printf("\n");
+
+        if (kick == secret_number) {
+            printf("Congratulations! The secret number is %d \n", secret_number);
+            break;
+        } else if (kick < secret_number) {
+            printf("The secret number is greater than %d \n", kick);
+        } else {
+            printf("The secret number is smaller than %d \n", kick);
+        }
+        attempts++;
+    }
+    return 0;
+}
+
